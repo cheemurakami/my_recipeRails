507af8dae6d64872619ef8c00b30e38a5f5b4ba9
Chisato Murakami <kiwipom1031@gmail.com>
2021-02-08 12:22:36 -0800

    remove ingredients column and fix controller and views

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -41,6 +41,6 @@ class RecipesController < ApplicationController
 
   private
     def recipe_params
-      params.require(:recipe).permit(:name, :category, :ingredients)
+      params.require(:recipe).permit(:name, :category)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_08_183228) do
+ActiveRecord::Schema.define(version: 2021_02_08_201744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 2021_02_08_183228) do
   create_table "recipes", force: :cascade do |t|
     t.string "name"
     t.string "category"
-    t.string "ingredients"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
