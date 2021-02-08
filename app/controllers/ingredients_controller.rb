803a3d016195d53b3fe2123157c58a59ea1553b4
Chisato Murakami <kiwipom1031@gmail.com>
2021-02-08 15:53:48 -0800

    add edit, update, and destroy method in ingredients

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -10,6 +10,28 @@ class IngredientsController < ApplicationController
     redirect_to recipe_path(@recipe)
   end
 
+  def edit
+    @recipe = Recipe.find(params[:recipe_id])
+    @ingredient = @recipe.ingredients.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:recipe_id])
+    @ingredient = Recipe.find(params[:id])
+    if  @ingredient.update(ingredients_params)
+      redirect_to recipes_path(@recipe)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:recipe_id])
+    @ingredient = @recipe.ingredients.find(params[:id])
+    @ingredient.destroy
+    redirect_to recipe_path(@recipe)
+  end
+  
   private
     def ingredients_params
       params.require(:ingredient).permit(:name, :measurement_unit, :measurement_qty, :recipe_id)
