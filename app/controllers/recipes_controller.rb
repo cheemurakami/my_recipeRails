7acf779038ca76de2e919f1e125049cc6f18e620
Chisato Murakami <kiwipom1031@gmail.com>
2021-02-08 11:26:45 -0800

    add edit link and view page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,4 +6,15 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  
+
+  private
+    def recipe_params
+      params.require(:recipe).permit(:name, :category, :ingredients)
+    end
 end
