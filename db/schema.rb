47b02ebce79f02aff506cd7bca66f3947acc4f74
Chisato Murakami <kiwipom1031@gmail.com>
2021-02-10 15:21:44 -0800

    add steps table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_08_211350) do
+ActiveRecord::Schema.define(version: 2021_02_10_231835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,4 +32,14 @@ ActiveRecord::Schema.define(version: 2021_02_08_211350) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "steps", force: :cascade do |t|
+    t.integer "index"
+    t.text "description"
+    t.bigint "recipe_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["recipe_id"], name: "index_steps_on_recipe_id"
+  end
+
+  add_foreign_key "steps", "recipes"
 end
