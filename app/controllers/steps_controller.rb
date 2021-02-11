4238efa8a3804a875564fb5ab99314b64a9504e3
Chisato Murakami <kiwipom1031@gmail.com>
2021-02-10 16:31:37 -0800

    fix step params

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -6,11 +6,16 @@ class StepsController < ApplicationController
 
   def create
     @recipe = Recipe.find(params[:recipe_id])
-    @step = Step.find(params[:id])
+    @step = Step.new(step_params)
+    if @step.save
+      redirect_to recipe_path(@recipe), notice: "Step is successfully added!"
+    else
+      render 'new'
+    end
   end
 
   private
     def step_params
-      params.require(:step).permit(:index, :description)
+      params.require(:step).permit(:index, :description, :recipe_id)
     end
 end
