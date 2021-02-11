715b5aa31fb505e717f84a4d5269920194335c97
Chisato Murakami <kiwipom1031@gmail.com>
2021-02-11 15:20:26 -0800

    fix search method to be able to search from 2 different columns

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,6 +6,13 @@ class Recipe < ApplicationRecord
   scope :three_most_recent, -> {order(created_at: :desc).limit(3)}
 
   def self.search(search)
-    Recipe.where("name ILIKE ?", "%#{search}%")
+    Recipe.where("name ILIKE ? or category ILIKE ?", "%#{search}%", "%#{search}%")
   end
+  # if you write this as scope
+  # scope :search, -> (search) {where("name ILIKE ? or category ILIKE ?", "%#{search}%", "%#{search}%")}
+  
+  # to get record from only one column
+   #Recipe.where("name ILIKE ?", "%#{search}%")
+  # to get record from only one column(the other way)
+   # Recipe.where('name ILIKE :search OR category ILIKE :search', search: "%#{search}%")
 end
