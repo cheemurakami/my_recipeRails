22e9399ad5eb8baf7ecde2b5e95b3ba1a53dcdcb
Chisato Murakami <kiwipom1031@gmail.com>
2021-02-10 16:25:09 -0800

    added new and create action with step params

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -1,2 +1,16 @@
 class StepsController < ApplicationController
+  def new
+    @recipe = Recipe.find(params[:recipe_id])
+    @step = Step.new(recipe_id: params[:recipe_id])
+  end
+
+  def create
+    @recipe = Recipe.find(params[:recipe_id])
+    @step = Step.find(params[:id])
+  end
+
+  private
+    def step_params
+      params.require(:step).permit(:index, :description)
+    end
 end
