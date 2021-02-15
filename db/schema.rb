efedf3555b9e495a1025c7e9c430a35dac5e1e37
Chisato Murakami <kiwipom1031@gmail.com>
2021-02-14 16:53:02 -0800

    add review table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_10_231835) do
+ActiveRecord::Schema.define(version: 2021_02_15_004630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,15 @@ ActiveRecord::Schema.define(version: 2021_02_10_231835) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "comment"
+    t.integer "star"
+    t.bigint "recipe_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["recipe_id"], name: "index_reviews_on_recipe_id"
+  end
+
   create_table "steps", force: :cascade do |t|
     t.integer "index"
     t.text "description"
@@ -41,5 +50,6 @@ ActiveRecord::Schema.define(version: 2021_02_10_231835) do
     t.index ["recipe_id"], name: "index_steps_on_recipe_id"
   end
 
+  add_foreign_key "reviews", "recipes"
   add_foreign_key "steps", "recipes"
 end
